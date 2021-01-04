4225fcae5c76917a7d24aad2894e09965ac24fdd
junokili <junokili@hotmail.com>
2021-01-04 12:11:39 +0000

    added basic tour page to check database and modified base template and footer

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Tour
+
+
+def all_tours(request):
+
+    tours = Tour.objects.all()
+
+    context = {
+        'tours': tours,
+    }
+
+    return render(request, 'tours/tours.html', context)
