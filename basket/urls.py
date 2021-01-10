e8f5def8c3d4d505d4dc0003f7163e0a768d96ec
junokili <junokili@hotmail.com>
2021-01-10 12:25:30 +0000

    can edit quantity and remove items from basket, ongoing problem with quantity being doubled...

--- basket/urls.py
+++ basket/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add/<tour_id>/', views.add_to_basket, name='add_to_basket'),
+    path('edit/<tour_id>/', views.edit_basket, name='edit_basket'),
+    path('remove/<tour_id>/', views.remove_from_basket, name='remove_from_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.contrib import messages
+
+from tours.models import Tour
 
 # Create your views here.
 
@@ -20,5 +23,36 @@ def add_to_basket(request, tour_id):
         basket[tour_id] = quantity
 
     request.session['basket'] = basket
-    print
     return redirect(redirect_url)
+
+
+def edit_basket(request, tour_id):
+
+    tour = get_object_or_404(Tour, pk=tour_id)
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if quantity > 0:
+        basket[tour_id] = quantity
+        messages.success(request, f'Updated {tour.name} quantity to {basket[tour_id]}')
+    else:
+        basket.pop(tour_id)
+        messages.success(request, f'Removed {tour.name} from your bag')
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, tour_id):
+
+    try:
+        basket = request.session.get('basket', {})
+
+        basket.pop(tour_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
