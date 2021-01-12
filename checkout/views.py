f988074cb6e4247be9ad6555c5455b6bf1dd29a6
junokili <junokili@hotmail.com>
2021-01-12 04:04:09 +0000

    stripe elements handle form submit added

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
-# Create your views here.
+from basket.contexts import basket_contains
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "Your shopping basket is currently empty")
         return redirect(reverse('tours'))
 
+    current_basket = basket_contains(request)
+    total = current_basket['grand_total']
     order_form = OrderForm()
     template = 'checkout/checkout.html'
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Ht9HFLpEsCk7L28N5hR3qGSv0j2IUoaYHD3hd6ZQb2Rg2SylJNJMFqsoEoyayMhHmrysr1IzV9XO2iFjcwSWhMu00H5ILhZjW',
-        'client_secret': 'test',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
