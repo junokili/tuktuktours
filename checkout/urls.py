30c4865c08a5dd0126a7b3472eb938e0552cefd4
junokili <junokili@hotmail.com>
2021-01-15 12:56:58 +0000

    cache billing data function added

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,6 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('successful_checkout/<order_number>/', views.successful_checkout, name='successful_checkout'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
