fada9184bd45d70c6d4fb0b8305444de80e3b03d
junokili <junokili@hotmail.com>
2021-01-31 11:32:15 +0000

    corrected issue in contact admin so messages can be viewed in the admin console and 500 is not returned

--- contact/admin.py
+++ contact/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Message
 
 
-class CommentAdmin(admin.ModelAdmin):
+class MessageAdmin(admin.ModelAdmin):
     list_display = ('message_email', 'message_author', 'message_body',
                     'writtenon',)
 
     ordering = ('writtenon',)
 
 
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
